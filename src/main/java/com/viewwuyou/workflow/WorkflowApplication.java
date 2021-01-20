9ef7dc6560d1a2d3edf7e79619c856b6e6cceef6
viewwuyou <viewwuyou@gmail.com>
2021-01-20 20:05:08 +0800

    更改mybatis包的引用

--- src/main/java/com/viewwuyou/workflow/WorkflowApplication.java
+++ src/main/java/com/viewwuyou/workflow/WorkflowApplication.java
@@ -1,13 +1,18 @@
 package com.viewwuyou.workflow;
 
+import com.viewwuyou.workflow.dao.EmployeeMapper;
+import com.viewwuyou.workflow.domain.Employee;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
+@MapperScan("com.viewwuyou.workflow.dao")
 @SpringBootApplication
 public class WorkflowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorkflowApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/viewwuyou/workflow/controller/mybatisTestController.java
@@ -0,0 +1,25 @@
+package com.viewwuyou.workflow.controller;
+
+import com.viewwuyou.workflow.dao.EmployeeMapper;
+import com.viewwuyou.workflow.domain.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@Controller
+public class mybatisTestController {
+    @Resource
+    private EmployeeMapper employeeMapper;
+
+    @RequestMapping("/mybatis")
+    @ResponseBody
+    public String employee() {
+        Employee employee = employeeMapper.get(1);
+        return employee.getName()+employee.getPass();
+    }
+
+}
